ec865ba4e99916dfd2b3672367312a92a7edd814
ZrigHiba <44437902+med-aziz-ben-haha@users.noreply.github.com>
2023-05-03 12:21:49 +0100

    creation

---
+++ src/main/java/com/ooredoo/unified_dashboad.java
@@ -0,0 +1,16 @@
+package com.ooredoo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@EnableScheduling
+@SpringBootApplication
+public class unified_dashboad {
+
+	public static void main(String[] args) {
+		SpringApplication.run(unified_dashboad.class, args);
+}
+}
